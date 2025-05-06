996b6782b27d340cbc0f87600a3f250110a9cb93
Pratikraj Mane <148538882+Pratikraj09@users.noreply.github.com>
2025-05-06 19:33:39 +0530

    Update settings.py

--- pratik/settings.py
+++ pratik/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--*%3-(d9#an8mi*3xtfo=q4)#$ug4i=dyqju7$*^k50t5fbxqm'
+SECRET_KEY = 'YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
